2be3247bdecd373e1a08d7caea982c48d2d39681
Maciek-Hetman <66074902+Maciek-Hetman@users.noreply.github.com>
2026-06-25 21:27:30 +0200

    update Lens data class

--- app/src/main/java/com/mobilelens/mobilelens/data/Lens.kt
+++ app/src/main/java/com/mobilelens/mobilelens/data/Lens.kt
@@ -12,14 +12,22 @@ enum class Stabilization {
     SENSORSHIFT,
 }
 
+data class VideoResolution(
+    val width: Int,
+    val height: Int,
+    val fps: Int
+) {
+    override fun toString() : String {
+        return "${width}x${height}@$fps"
+    }
+}
+
 data class Lens(
     val id: String,
     val brand: String,
     val model: String,
-    val focalLength: Double,
-    val focalLength35mm: Double,
-    val aperture: Double,
-    val aperture35mm: Double,
+    val focalLength: List<Double>,
+    val aperture: List<Double>,
     val cropFactor: Double,
     val facing: Facing,
     val pixelPitchUm: Double,
@@ -27,6 +35,8 @@ data class Lens(
     val activeResolution: Double,
     val afZones: Int,
     val ois: Stabilization,
+    val videoResolutions: List<VideoResolution>
 ) {
-
+    val focalLength35mm = focalLength.map { it * cropFactor }
+    val aperture35mm = aperture.map { it * cropFactor }
 }
