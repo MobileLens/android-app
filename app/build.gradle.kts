6f3de7f384bb8d667d9e6098465ebb26d751b1e4
Maciek-Hetman <66074902+Maciek-Hetman@users.noreply.github.com>
2026-09-21 17:59:10 +0200

    remove duplicate dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,6 @@ android {
 }
 
 dependencies {
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.runtime)
